7c60038a70f1e5ccfe4fb2dfae168111e68ef4b4
Gunel <dibirovagunel5@gmail.com>
2024-01-04 04:26:35 +0400

    Finished

--- src/ContactMePage.js
+++ src/ContactMePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './ContactMe.css'; // Import a specific CSS file for ContactMePage
+import './ContactMe.css'; 
 
 const ContactMePage = () => {
   const [subject, setSubject] = useState('');
@@ -9,7 +9,7 @@ const ContactMePage = () => {
 
   const handleSendMessage = async () => {
     try {
-      await axios.post('http://localhost:3001/messages', { subject, email, content });
+      await axios.post('http://localhost:3501/messages', { subject, email, content });
       console.log('Message sent successfully');
       setSubject('');
       setEmail('');
@@ -20,7 +20,7 @@ const ContactMePage = () => {
   };
 
   return (
-    <div className="contact-me-page"> {/* Add a class to the root element */}
+    <div className="contact-me-page"> 
       <h2>Here Is Contact Me Page!</h2>
       <label>Subject: <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} /></label>
       <label>Email: <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /></label>
